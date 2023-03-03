cb98635a0fbd01d31760f90cb0129e8bb3c7072a
jovan <lisanchukivan@gmail.com>
2023-03-03 20:59:40 +0300

    task №2

---
+++ lesson1/task2.py
@@ -0,0 +1,7 @@
+a, b = float(input('Первое число: ')), float(input('Второе число: '))
+if a == b:
+    print('Числа равны')
+elif a > b:
+    print('Первое число больше')
+elif b > a:
+    print('Второе число больше')
